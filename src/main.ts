b03517c86a0e9a851003ad6e45d75eda63bee79b
zied3332 <141698161+zied3332@users.noreply.github.com>
2026-07-19 02:01:39 +0100

    configure Prisma and environment variables

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const configService = app.get(ConfigService);
+
+  const port = configService.get<number>('PORT', 3000);
+
+  await app.listen(port);
+
+  console.log(`Waterfall Festival API running on http://localhost:${port}`);
 }
+
 bootstrap();
