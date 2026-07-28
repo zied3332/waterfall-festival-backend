7e27f5cb667b6721793a4d521f708f452ee1936a
zied3332 <141698161+zied3332@users.noreply.github.com>
2026-07-28 16:20:45 +0100

    feat(tickets): add public ticket controller

--- src/tickets/tickets.controller.ts
+++ src/tickets/tickets.controller.ts
@@ -1,4 +1,31 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  ParseIntPipe,
+  Query,
+} from '@nestjs/common';
+
+import { TicketQueryDto } from './dto/ticket-query.dto.js';
+import { TicketsService } from './tickets.service.js';
 
 @Controller('tickets')
-export class TicketsController {}
+export class TicketsController {
+  constructor(
+    private readonly ticketsService: TicketsService,
+  ) {}
+
+  @Get()
+  findAll(
+    @Query() query: TicketQueryDto,
+  ) {
+    return this.ticketsService.findAll(query);
+  }
+
+  @Get(':id')
+  findOne(
+    @Param('id', ParseIntPipe) id: number,
+  ) {
+    return this.ticketsService.findOne(id);
+  }
+}
