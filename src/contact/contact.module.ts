dc54622d02ac628588cc44f80787926b2d013338
zied3332 <141698161+zied3332@users.noreply.github.com>
2026-07-28 11:35:09 +0100

    create the mail module and updated teh contact module

--- src/contact/contact.module.ts
+++ src/contact/contact.module.ts
@@ -1,9 +1,8 @@
 import { Module } from '@nestjs/common';
 
-import { AuthModule } from '../auth/auth.module.js';
+import { MailModule } from '../mail/mail.module.js';
 import { NotificationsModule } from '../notifications/notifications.module.js';
 import { PrismaModule } from '../prisma/prisma.module.js';
-import { AdminMessagesController } from './admin-messages.controller.js';
 import { ContactController } from './contact.controller.js';
 import { ContactService } from './contact.service.js';
 
@@ -11,10 +10,9 @@ import { ContactService } from './contact.service.js';
   imports: [
     PrismaModule,
     NotificationsModule,
-    AuthModule,
+    MailModule,
   ],
-  controllers: [ContactController, AdminMessagesController],
+  controllers: [ContactController],
   providers: [ContactService],
-  exports: [ContactService],
 })
 export class ContactModule {}
