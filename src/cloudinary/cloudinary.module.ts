16ee117f9b21e1f0941232a0ebddd8fe0044b28f
zied3332 <141698161+zied3332@users.noreply.github.com>
2026-07-30 14:51:32 +0100

    refactor(cloudinary): format Cloudinary module

--- src/cloudinary/cloudinary.module.ts
+++ src/cloudinary/cloudinary.module.ts
@@ -1,9 +1,13 @@
 import { Module } from "@nestjs/common";
+
 import { CloudinaryProvider } from "./cloudinary.provider.js";
 import { CloudinaryService } from "./cloudinary.service.js";
 
 @Module({
-  providers: [CloudinaryProvider, CloudinaryService],
+  providers: [
+    CloudinaryProvider,
+    CloudinaryService,
+  ],
   exports: [CloudinaryService],
 })
 export class CloudinaryModule {}
