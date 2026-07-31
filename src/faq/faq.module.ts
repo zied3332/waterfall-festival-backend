91971a5826017cc6e690cdded0e2d8b8321a7143
zied3332 <141698161+zied3332@users.noreply.github.com>
2026-07-31 21:28:32 +0100

    fix(faq): provide authentication guards to FAQ module

--- src/faq/faq.module.ts
+++ src/faq/faq.module.ts
@@ -1,14 +1,26 @@
 import { Module } from "@nestjs/common";
 
+import { AuthModule } from "../auth/auth.module.js";
+import { PrismaModule } from "../prisma/prisma.module.js";
+
 import { AdminFaqController } from "./admin-faq.controller.js";
 import { FaqController } from "./faq.controller.js";
 import { FaqService } from "./faq.service.js";
 
 @Module({
+  imports: [
+    PrismaModule,
+    AuthModule,
+  ],
   controllers: [
     FaqController,
     AdminFaqController,
   ],
-  providers: [FaqService],
+  providers: [
+    FaqService,
+  ],
+  exports: [
+    FaqService,
+  ],
 })
 export class FaqModule {}
