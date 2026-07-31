cea3fc2ea3ecf8fa1d0ebcc8cacb269e34a99eb1
zied3332 <141698161+zied3332@users.noreply.github.com>
2026-07-31 09:03:35 +0100

    docs(gallery): document public gallery endpoint

--- src/gallery/gallery.controller.ts
+++ src/gallery/gallery.controller.ts
@@ -2,9 +2,16 @@ import {
   Controller,
   Get,
 } from "@nestjs/common";
+import {
+  ApiOkResponse,
+  ApiOperation,
+  ApiTags,
+} from "@nestjs/swagger";
 
+import { GalleryImageResponseDto } from "./dto/gallery-image-response.dto.js";
 import { GalleryService } from "./gallery.service.js";
 
+@ApiTags("Public Gallery")
 @Controller("gallery")
 export class GalleryController {
   constructor(
@@ -12,6 +19,18 @@ export class GalleryController {
   ) {}
 
   @Get()
+  @ApiOperation({
+    summary:
+      "List published gallery images",
+    description:
+      "Returns gallery images that are currently published on the public website. Images are ordered by sort order and then by creation date.",
+  })
+  @ApiOkResponse({
+    description:
+      "Published gallery images returned successfully.",
+    type: GalleryImageResponseDto,
+    isArray: true,
+  })
   findPublished() {
     return this.galleryService.findPublished();
   }
